77c78161dafd55638f5d27ed9856daa83ae74a34
Vinícius Santos <vncssnts31@gmail.com>
2025-03-15 12:12:20 -0300

    fix: modal não estava abrindo

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,35 +1,15 @@
-"use client";
-
 import { ConnectDatabaseDialog } from "@/components/pages/dashboard/database/connect-database-dialog";
 import {
   ActionCard,
   LinkActionCard,
 } from "@/components/pages/dashboard/database/database-card";
-import { Button } from "@/components/ui/button";
-import { useSidebar } from "@/components/ui/sidebar";
-import {
-  DatabaseZap,
-  FolderKey,
-  PanelLeft,
-  UserCog,
-  UsersRound,
-} from "lucide-react";
+import { PageTitle } from "@/components/shared/page-title";
+import { DatabaseZap, FolderKey, UserCog, UsersRound } from "lucide-react";
 
 export default function DashboardPage() {
-  const { toggleSidebar } = useSidebar();
-
   return (
     <>
-      <div className="mb-6 flex items-center justify-start gap-2">
-        <Button
-          variant={"ghost"}
-          className="!aspect-square"
-          onClick={toggleSidebar}
-        >
-          <PanelLeft size={32} className="!size-5" />
-        </Button>
-        <h1 className="font-title text-4xl font-bold">Dashboard</h1>
-      </div>
+      <PageTitle title="Dashboard" />
       <section className="flex w-full flex-wrap gap-3">
         <ConnectDatabaseDialog>
           <ActionCard
